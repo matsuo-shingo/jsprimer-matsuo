bca25620e1f114bb42c7a8c0149c17488eb305c2
matsuo-shingo <matsuo_shingo@retail-ai.jp>
2023-03-02 09:09:48 +0900

    todoアイテムの追加の章

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 console.log("App.js: loaded");
+import { element, render } from "./view/html-util.js";
+
 export class App {
-    constructor() {
-        console.log("App initialized");
+    mount() {
+        const formElement = document.querySelector("#js-form");
+        const inputElement = document.querySelector("#js-form-input");
+        const containerElement = document.querySelector("#js-todo-list");
+        const todoItemCountElement = document.querySelector("#js-todo-count");
+        const todoListElement = element`<ul></ul>`;
+        let todoItemCount = 0;
+        formElement.addEventListener("submit", (event) => {
+            event.preventDefault();
+            const todoItemElement = element`<li>${inputElement.value}</li>`;
+            todoListElement.appendChild(todoItemElement);
+            render(todoListElement, containerElement);
+            todoItemCount += 1;
+            todoItemCountElement.textContent = `Todoアイテム数: ${todoItemCount}`;
+            inputElement.value = "";
+        });
     }
 }
